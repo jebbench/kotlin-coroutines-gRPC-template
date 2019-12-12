04479657120a7209fc628e9ba70aaa407d60de9f
James Bench <james.bench@routethirtysix.com>
2019-12-12 13:44:51 +0000

    Replicate https://github.com/marcoferrer/kroto-plus/issues/93

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,18 +7,15 @@ import io.grpc.examples.helloworld.send
 import io.grpc.inprocess.InProcessChannelBuilder
 import io.grpc.inprocess.InProcessServerBuilder
 import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.consumeEach
 
 val server: Server = InProcessServerBuilder
     .forName("helloworld")
     .addService(GreeterService())
-    .directExecutor()
     .build()
     .start()
 
 val channel: Channel = InProcessChannelBuilder
     .forName("helloworld")
-    .directExecutor()
     .build()
 
 suspend fun main(){
@@ -28,59 +25,19 @@ suspend fun main(){
         .newStub(channel)
         .withCoroutineContext()
 
-    performUnaryCall(stub)
-
-    performBidiCall(stub)
-
     performClientStreamingCall(stub)
-
-    performServerStreamingCall(stub)
-
 
     server.shutdown()
 }
 
-suspend fun performUnaryCall(stub: GreeterCoroutineGrpc.GreeterCoroutineStub){
-
-    val unaryResponse = stub.sayHello { name = "John" }
-
-    println("Unary Response: ${unaryResponse.message}")
-}
-
-suspend fun performServerStreamingCall(stub: GreeterCoroutineGrpc.GreeterCoroutineStub){
-
-    val responseChannel = stub.sayHelloServerStreaming { name = "John" }
-
-    responseChannel.consumeEach {
-        println("Server Streaming Response: ${it.message}")
-    }
-}
 
 suspend fun performClientStreamingCall(stub: GreeterCoroutineGrpc.GreeterCoroutineStub) = coroutineScope{
 
     // Client Streaming RPC
     val (requestChannel, response) = stub.sayHelloClientStreaming()
 
-    launchProducerJob(requestChannel){
-        repeat(5){
-            send { name = "person #$it" }
-        }
-    }
+    requestChannel.send { name = "First" }
+    requestChannel.close(Exception("This is an exception!"))
 
     println("Client Streaming Response: ${response.await().toString().trim()}")
-}
-
-suspend fun performBidiCall(stub: GreeterCoroutineGrpc.GreeterCoroutineStub) = coroutineScope {
-
-    val (requestChannel, responseChannel) = stub.sayHelloStreaming()
-
-    launchProducerJob(requestChannel){
-        repeat(5){
-            send { name = "person #$it" }
-        }
-    }
-
-    responseChannel.consumeEach {
-        println("Bidi Response: ${it.message}")
-    }
 }
